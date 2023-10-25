58cc00811ebba3d3e6611e90f8ecac9ef5eac717
hackercat <brentobi703@gmail.com>
2023-10-25 10:14:30 -0400

    Initial upload

---
+++ src/main/java/ca/hackercat/logging/Logger.java
@@ -0,0 +1,139 @@
+package ca.hackercat.logging;
+
+import ca.hackercat.magma.io.Terminal;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public final class Logger {
+    private static PrintStream out = new PrintStream(new OutputStream() {
+        @Override
+        public void write(int b) throws IOException {
+            System.out.write(b);
+            Terminal.out.write(b);
+        }
+    });
+
+    private String className;
+
+    private static final String INFO_COLOR = "\u001b[0m";
+    private static final String WARN_COLOR = "\u001b[38;2;255;212;168m";
+    private static final String ERROR_COLOR = "\u001b[0;31m";
+    private static final String PROPERTIES_COLOR = "\u001b[0m";
+
+    private static final String RESET = "\u001b[0m";
+
+    private static boolean disabled = false;
+
+
+    private Logger(String className) {
+        this.className = className;
+    }
+
+    public static <T> Logger get(Class<T> clazz) {
+        return new Logger(clazz.getSimpleName());
+    }
+
+    private enum Level {
+        INFO,
+        WARN,
+        ERROR
+    }
+
+    public void log(Object o) {
+        log(o.toString());
+    }
+    public void log(Throwable t) {
+        log(t.toString() + "\n" + getStackTrace(t));
+    }
+    public void log(String msg) {
+        print(Level.INFO, msg);
+    }
+    public void warn(Object o) {
+        warn(o.toString());
+    }
+    public void warn(Throwable t) {
+        warn(t.toString() + "\n" + getStackTrace(t));
+    }
+    public void warn(String msg) {
+        print(Level.WARN, msg);
+    }
+    public void error(Object o) {
+        error(o.toString());
+    }
+    public void error(Throwable t) {
+        error(t.toString() + "\n" + getStackTrace(t));
+    }
+    public void error(String msg) {
+        print(Level.ERROR, msg);
+    }
+
+    private String getStackTrace(Throwable t) {
+        StackTraceElement[] elements = t.getStackTrace();
+        StringBuilder str = new StringBuilder();
+
+        for (StackTraceElement element : elements) {
+            str.append("    ").append(element.toString()).append("\n");
+        }
+        return str.toString();
+    }
+
+    private void print(Level level, String message) {
+        if (disabled)
+            return;
+
+        String thread = Thread.currentThread().getName();
+        String name = className;
+
+        String info = name.isBlank()? thread + "/Anonymous Class" : thread + "/" + name;
+
+        String logPrefix;
+        String color;
+
+        switch (level) {
+            case INFO -> {
+                logPrefix = "[" + getTime() + "] [INFO/" + info + "] ";
+                color = INFO_COLOR;
+            }
+            case WARN -> {
+                logPrefix = "[" + getTime() + "] [WARN/" + info + "] ";
+                color = WARN_COLOR;
+            }
+            case ERROR -> {
+                logPrefix = "[" + getTime() + "] [ERROR/" + info + "] ";
+                color = ERROR_COLOR;
+            }
+            default -> {
+                logPrefix = "";
+                color = "";
+            }
+        }
+
+        if (message.contains("\n")) {
+            StringBuilder replacement = new StringBuilder();
+            replacement.append("\n");
+            for (int i = 0, length = logPrefix.length(); i < length; i++) {
+                replacement.append(" ");
+            }
+            message = message.replaceAll("\n", replacement.toString());
+        }
+
+        out.println(color + logPrefix + message + RESET);
+    }
+
+    private final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private String getTime() {
+        return dtf.format(LocalDateTime.now());
+    }
+
+    public static void disableAllLoggers() {
+//        out.println(ERROR_COLOR + "DISABLING LOGGERS! This should ONLY be used for debugging " +
+//                "if a different logging API is using System.out, eg. the LWJGLX debugger.");
+        disabled = true;
+    }
+}
